e6540c4c023da48b98809a8af70a998d04b031fd
Rahmi Esen <esenrahmi23@gmail.com>
2025-09-02 11:08:07 +0200

    feat(browse): connect and structure page components

--- src/app/features/browse/components/movie-list/movie-list.component.ts
+++ src/app/features/browse/components/movie-list/movie-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ListHeaderComponent } from './list-header/list-header.component';
+import { MovieCardComponent } from '../../../../shared/components/movie-card/movie-card.component';
 
 @Component({
   selector: 'app-movie-list',
-  imports: [],
+  imports: [ListHeaderComponent, MovieCardComponent],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss',
 })
--- src/app/features/browse/pages/browse/browse.component.ts
+++ src/app/features/browse/pages/browse/browse.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroBannerComponent } from '../../components/hero-banner/hero-banner.component';
+import { MovieListComponent } from '../../components/movie-list/movie-list.component';
 
 @Component({
   selector: 'app-browse',
-  imports: [],
+  standalone: true,
+  imports: [HeroBannerComponent, MovieListComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss',
 })
