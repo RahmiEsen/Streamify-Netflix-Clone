65aae8c91a18a4149c0e30b30d4cfe16922cff92
Rahmi Esen <esenrahmi23@gmail.com>
2025-09-03 05:57:01 +0200

    feat: Add controls and rating badge to hero banner

---
+++ src/app/shared/components/hero-banner/age-rating/age-rating.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { IconButtonComponent } from '../../icon-button/icon-button.component';
+
+@Component({
+  selector: 'app-age-rating',
+  imports: [IconButtonComponent],
+  templateUrl: './age-rating.component.html',
+  styleUrl: './age-rating.component.scss',
+})
+export class AgeRatingComponent {}
--- src/app/shared/components/hero-banner/hero-banner.component.ts
+++ src/app/shared/components/hero-banner/hero-banner.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroMetaComponent } from './hero-meta/hero-meta.component';
+import { AgeRatingComponent } from './age-rating/age-rating.component';
 
 @Component({
   selector: 'app-hero-banner',
   standalone: true,
-  imports: [HeroMetaComponent],
+  imports: [HeroMetaComponent, AgeRatingComponent],
   templateUrl: './hero-banner.component.html',
   styleUrl: './hero-banner.component.scss',
 })
