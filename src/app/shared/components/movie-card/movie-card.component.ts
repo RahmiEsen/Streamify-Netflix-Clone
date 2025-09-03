f7f1137d4982351f64982dc100472fc04757bf2c
Rahmi Esen <esenrahmi23@gmail.com>
2025-09-03 17:04:02 +0200

    refactor(movie-list): Implement data-driven rendering for movie cards using mock data.

--- src/app/shared/components/movie-card/movie-card.component.ts
+++ src/app/shared/components/movie-card/movie-card.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Movie } from '../../../core/models/movie.model';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MovieCardComponent {}
+export class MovieCardComponent {
+  @Input({ required: true }) movie!: Movie;
+}
--- src/app/shared/components/movie-list/movie-list.component.ts
+++ src/app/shared/components/movie-list/movie-list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { ListHeaderComponent } from './list-header/list-header.component';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { CommonModule } from '@angular/common';
+import { Movie } from '../../../core/models/movie.model';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [ListHeaderComponent, MovieCardComponent],
+  imports: [CommonModule, ListHeaderComponent, MovieCardComponent],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss',
 })
-export class MovieListComponent {}
+export class MovieListComponent {
+  movies: Movie[] = [
+    { id: 1, title: 'Show 1', imageUrl: 'assets/shows/1.webp' },
+    { id: 2, title: 'Show 2', imageUrl: 'assets/shows/2.webp' },
+    { id: 3, title: 'Show 3', imageUrl: 'assets/shows/3.webp' },
+    { id: 4, title: 'Show 4', imageUrl: 'assets/shows/4.webp' },
+    { id: 5, title: 'Show 5', imageUrl: 'assets/shows/5.jpg' },
+    { id: 6, title: 'Show 6', imageUrl: 'assets/shows/6.jpg' },
+    { id: 6, title: 'Show 7', imageUrl: 'assets/shows/7.webp' },
+  ];
+}
