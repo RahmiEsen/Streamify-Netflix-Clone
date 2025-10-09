acd9e08b6b6bbdeff289955e68d4b3048682dcf7
Rahmi Esen <esenrahmi23@gmail.com>
2025-10-09 08:13:50 +0200

    fix(deployment): Remove process.env to resolve browser ReferenceError

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl:
-    process.env['NG_APP_BACKEND_URL'] ||
-    'https://streamify-netflix-clone.onrender.com',
+  backendUrl: 'https://streamify-netflix-clone.onrender.com',
 };
