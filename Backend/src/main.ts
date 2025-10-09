a3fb1b78b5be4e124d559c2df458e2eaec25ade9
Rahmi Esen <esenrahmi23@gmail.com>
2025-10-09 06:00:46 +0200

    feat(backend): Configure CORS and dynamic port for production

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'https://DEINE_FRONTEND_URL.vercel.app', // Später hier eintragen
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
